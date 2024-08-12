693c4da899335d1f9f4441dbb797314c6b739499
Wilgas ADJOVI <Wilfrieda216@gmail.com>
2024-08-12 18:27:44 +0200

    fillable message

--- app/Models/Message.php
+++ app/Models/Message.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class Message extends Model
 {
     use HasFactory;
+     /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'message',
+        'sender_id',
+        'receiver_id',
+        'group_id',
+        'conversation_id',
+    ];
 }
