d0cbd88e001e349b65da794fdedc999aac02203e
Wilgas ADJOVI <Wilfrieda216@gmail.com>
2024-08-12 12:19:28 +0200

    users migration crééer

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -14,10 +14,13 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('avatar')->nullable();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
+            $table->boolean('is_admin')->default(false);
+            $table->timestamp('blocked_at')->nullable();
             $table->timestamps();
         });
 
