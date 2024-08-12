eb82b4b7f00a05fdf39c71615ddf92ada16b7d6a
Wilgas ADJOVI <Wilfrieda216@gmail.com>
2024-08-12 18:21:41 +0200

    fillabe user

--- app/Models/User.php
+++ app/Models/User.php
@@ -18,8 +18,11 @@ class User extends Authenticatable
      */
     protected $fillable = [
         'name',
+        'avatar',
         'email',
         'password',
+        'is_admin',
+        'blocked_at',
     ];
 
     /**
