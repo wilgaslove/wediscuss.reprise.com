183fc2a0f5e1a6655b65a86fed0d1dafaa7cd81e
Wilgas ADJOVI <Wilfrieda216@gmail.com>
2024-08-12 18:06:59 +0200

    group  migration messageAttachement

--- database/migrations/2024_08_12_100906_create_message_attachements_table.php
+++ database/migrations/2024_08_12_100906_create_message_attachements_table.php
@@ -13,6 +13,11 @@ return new class extends Migration
     {
         Schema::create('message_attachements', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('message_id')->constrained('messages');
+            $table->string('name', 255); // x2dTiodfseerfeoij.jpg
+            $table->string('path', 1024);// /images/x2dTiodfseerfeoij.jpg
+            $table->string('nime', 255);// jpg, png, xsl
+            $table->integer('size');  // taille du fichier
             $table->timestamps();
         });
     }
