7e4e9af6cb07bb5d96a76f3d72de48551acbe776
Wilgas ADJOVI <Wilfrieda216@gmail.com>
2024-08-12 18:32:40 +0200

    fillable conversation

--- app/Models/Conversation.php
+++ app/Models/Conversation.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class Conversation extends Model
 {
     use HasFactory;
+     /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'user_id1',
+        'user_id2',
+        'last_message_id',
+    ];
 }
