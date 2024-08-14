a9c3258cea56f56f9918f6d64bde27bb6577b781
Wilgas ADJOVI <Wilfrieda216@gmail.com>
2024-08-14 17:56:37 +0200

    model user.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,7 +2,6 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -12,52 +11,61 @@ class User extends Authenticatable
     use HasFactory, Notifiable;
 
     /**
-     * The attributes that are mass assignable.
+     * Les attributs qui peuvent être assignés en masse.
      *
      * @var array<int, string>
      */
     protected $fillable = [
-        'name',
-        'avatar',
-        'email',
-        'password',
-        'is_admin',
-        'blocked_at',
+        'name',         // Nom de l'utilisateur
+        'avatar',       // URL de l'avatar de l'utilisateur
+        'email',        // Adresse email de l'utilisateur
+        'password',     // Mot de passe de l'utilisateur (crypté)
+        'is_admin',     // Indicateur si l'utilisateur est administrateur
+        'blocked_at',   // Date de blocage de l'utilisateur (si applicable)
     ];
 
     /**
-     * The attributes that should be hidden for serialization.
+     * Les attributs qui doivent être masqués lors de la sérialisation.
      *
      * @var array<int, string>
      */
     protected $hidden = [
-        'password',
-        'remember_token',
+        'password',        // Masquer le mot de passe lors de la sérialisation
+        'remember_token',  // Masquer le token de "se souvenir" lors de la sérialisation
     ];
 
     /**
-     * Get the attributes that should be cast.
+     * Les attributs qui doivent être castés en types spécifiques.
      *
      * @return array<string, string>
      */
     protected function casts(): array
     {
         return [
-            'email_verified_at' => 'datetime',
-            'password' => 'hashed',
+            'email_verified_at' => 'datetime',  // Convertir l'attribut email_verified_at en instance de DateTime
+            'password' => 'hashed',            // Convertir l'attribut password en chaîne de caractères hachée
         ];
     }
 
-
     /**
-     * Un user appartient  à plusieurs Groups
+     * Définir la relation "appartient à plusieurs" entre User et Group.
+     * Un utilisateur peut appartenir à plusieurs groupes et un groupe peut contenir plusieurs utilisateurs.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
      */
-    public function groups() {
-        // return $this->belongsToMany(Group::class, 'group_user', 'user_id', 'group_id');
-        return $this->belongsToMany(Group::class, 'group_user');// On peut écrire des deux manières
+    public function groups()
+    {
+        return $this->belongsToMany(Group::class, 'group_user');  // La table pivot est group_user
     }
 
-    public function messages() {
-        return $this->hasMany(Message::class);
+    /**
+     * Définir la relation "a plusieurs" entre User et Message.
+     * Un utilisateur peut avoir plusieurs messages.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function messages()
+    {
+        return $this->hasMany(Message::class);  // La relation est définie sur la clé étrangère user_id dans la table messages
     }
 }
