c1636fadd3cf336b9a35fbfa89fd31709a3b0dae
Wilgas ADJOVI <Wilfrieda216@gmail.com>
2024-08-20 20:29:59 +0200

    userFac

--- database/factories/GroupFactory.php
+++ database/factories/GroupFactory.php
@@ -17,9 +17,9 @@ class GroupFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => fake()->name(),
-            'description' =>fake()->sentence(),
-           
+            'name' => fake()->name() ,
+            'description' => fake()->sentence() ,
+  
         ];
     }
 }
