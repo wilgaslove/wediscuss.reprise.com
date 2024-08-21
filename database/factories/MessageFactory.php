3436bfdf12d4be9ca0424ffd9ef4ec5fb6adef37
Wilgas ADJOVI <Wilfrieda216@gmail.com>
2024-08-21 13:15:27 +0200

    messageFactory

--- database/factories/MessageFactory.php
+++ database/factories/MessageFactory.php
@@ -16,17 +16,61 @@ class MessageFactory extends Factory
      */
     public function definition(): array
     {
-        $senderId = fake()->randomElement([0, 1]);
-        if ($senderId === 0) {
-            $senderId = fake()->randomElement(\App\Models\User::where('id', '!=', 1)->pluck('id')->toArry());
-            $receiverId = 1;
+        //On sélectionne de manière un utilisateur
+        $userIds = \App\Models\User::pluck('id')->toArray();
+
+        //On décide de manière aléatoire si le message est un message direct (conversation) ou un message de group
+        $isGroupMessage = fake()->boolean(50);
+
+        // On sélectionn un sender aléatoirement
+        $senderId = fake()->randomElement($userIds);
+
+        // On initialise le receiverId et le groupId
+        $receiverId = null;
+        $groupId = null;
+
+       
+     // Ceci est un message de groupe
+     if ($isGroupMessage) {
+        # On s'assure que le groupe exist dansle BDD
+        $groupIds = \App\Models\Group::pluck('id')->toArray();
+
+        if (empty($groupIds)) {
+            throw new \Exception("Aucun groupe trouvé dans la base de donnée");
         }
+        //on prend au hazard un groupe
+        $groupId = fake()->randomElement($groupIds);
+
+        //Sélectionnner un groupe aléatoirement
+        $group = \App\Models\Group::find($groupId);
+        # On récupère un utilisateur du groupe  aléatoirement
+        $senderId = fake()->randomElement($group->users->pluck('id')->toArray());
+    } else {
+        // C'est un message direct qu'on envoie
+        //Sélectionner un receiver qui est différent du sender
+        $receiverId = fake()->randomElement(array_diff($userIds, [$senderId])); // array_diff([1,2,3,4,5], [3]) => [1, 2, 4, 5]
+    }
+
+     if (!$isGroupMessage) {
+        $conversationId = \App\Models\Conversation::firstOrCreate([
+            'user_id1' => min($senderId, $receiverId),
+            'user_id2' => max($senderId, $receiverId),
+        ],
+        [
+            'last_message_id' => null,
+        ]
+        
+    );
+     }
+
+    
+        // Retourne un tableau associatif avec les données générées pour un message.
         return [
-            'message' => fake()->realText(),
-            // 'sender_id' =>fake()-> '' ,
-            // 'receiver_id' =>fake()-> '',
-            // 'group_id' =>fake()-> '',
-            // 'conversation_id' =>fake()-> '',
+            'message' => fake()->realText(),  // Génère un texte aléatoire pour le message.
+            'sender_id' => $senderId,         // ID de l'expéditeur du message.
+            'receiver_id' => $receiverId,     // ID du destinataire du message.
+            'group_id' => $groupId,           // ID du groupe (peut être null si aucun groupe n'a été sélectionné).
+            'conversation_id' => $conversationId,          // ID de la conversation (actuellement vide).
         ];
     }
 }
